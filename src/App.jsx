95bf452c1553a5edc8155f24ee98f3a10fbf8164
Sparshagarwal29 <sparshagarwal2909@gmail.com>
2025-07-02 10:47:57 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import { useState } from 'react'
+// import './App.css'
+
+function App() {
+
+return (
+    <>
+        <div>
+            <h1>Expense Tracker</h1>
+            <input type="text" />
+            <button>Add</button>
+            <div>
+                <p>food</p>
+                <button>delete</button>
+                
+            </div>
+        </div>
+    </>
+)
+}
+
+export default App
